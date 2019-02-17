9f87ae4f3caff82949654ce76ea4e52ed44c3cea
Mayeul Fournial <mfournial@outlook.com>
2019-02-17 18:15:55 +0000

    Basic VGA printing class

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga;
+
+// static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,14 +14,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    vga::print_something();
     loop {}
 }
