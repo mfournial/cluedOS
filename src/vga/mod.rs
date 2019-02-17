2fc3e4b540b719e70e48094017cce6b1cf3bbb4d
Mayeul Fournial <mfournial@outlook.com>
2019-02-17 18:36:15 +0000

    Use print macros in main

--- src/vga/mod.rs
+++ src/vga/mod.rs
@@ -128,22 +128,9 @@ lazy_static! {
     });
 }
 
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
-}
-
 #[macro_export]
 macro_rules! print {
-    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
 }
 
 #[macro_export]
